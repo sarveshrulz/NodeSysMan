d77a4265fd7a21ebe632738eb40c199021f15912
sarveshrulz <sarveshkardekar@gmail.com>
2022-04-03 08:30:58 +0530

    Authenticate and run commands via SSH

--- server.js
+++ server.js
@@ -1,29 +1,82 @@
-const app = require("express")();
+const express = require("express")
+const app = express()
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const path = require("path");
-const { exec } = require("child_process");
-const port = process.env.PORT || 3000;
+const path = require("path")
+const port = process.env.PORT || 3000
+const {NodeSSH} = require("node-ssh")
 
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "src/index.html"));
-});
+app.use(express.urlencoded({ extended: true }))
 
-io.on("connection", socket => {
-    console.log("A user connected");
-    // socket.on("message", msg => {
-    //     console.log("Client msg:", msg);
-    // });
-    setInterval(() => {
-        exec("free -m", (error, stdout, stderror) => {
-            socket.emit("server", stdout);
-        });
-    }, 300)
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.get("/info", (req, res) => {
+    res.sendFile(path.join(__dirname, "info.html"));
+})
+
+app.post("/", (req, res) => {
+    const sshAuth = new NodeSSH()
+
+    host = {
+        host: "localhost",
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    sshAuth.connect(host).then(() => {
+        res.redirect("/info")
+        sshAuth.dispose()
+    }).catch((err) => {
+        res.redirect("/")
+        setTimeout(() => {
+            io.of("/").emit("authFailed", err.toString("utf-8") + ", please try again...")
+        }, 200)
+    })
+})
+
+io.of("/").on("connect", (socket) => {
+    console.log("Connected to /")
+
     socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
-});
+        console.log("Disconnected from /")
+    })
+})
+
+io.of("/info").on("connect", (socket) => {
+    const sshInfo = new NodeSSH()
+
+    console.log("Connected on /info")
+
+    if (typeof host != "undefined") {
+        sshInfo.connect(host).then(() => {
+            let interval = setInterval(() => {
+                if (sshInfo.isConnected()) {
+                    sshInfo.execCommand("cat /etc/hostname").then((result) => {
+                        socket.emit("hostname", result.stdout)
+                    })
+                    sshInfo.execCommand("uptime | awk '{ print $3 $4}'").then((result) => {
+                        socket.emit("uptime", result.stdout)
+                    })
+                    sshInfo.execCommand("free -m").then((result) => {
+                        socket.emit("memory", result.stdout)
+                    })
+                } else {
+                    clearInterval(interval)
+                }
+            }, 300)
+        })
+    } else {
+        socket.emit("redirectRoot")
+    }
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected from /info")
+        sshInfo.dispose()
+    })
+})
 
 http.listen(port, () => {
-    console.log("Server listening on port", port);
-});
+    console.log("Server listening on port", port)
+})
